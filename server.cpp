1d0cf1f46f20e2251418a1c21b9800928c939600
logo <logo-github2@talkat.us>
2021-04-08 16:13:20 -0700

    Continuing to add functionality.

--- server.cpp
+++ server.cpp
@@ -9,9 +9,22 @@
 #include <sys/uio.h>      // writev
 #include <sys/time.h>     //gettimeofday
 #include <string>         //string
+#include <pthread.h>      //Pthread
 #include <iostream>
 
 using namespace std;
+
+//Global Constant
+const int BUFFERSIZE = INT32_MAX;
+
+//Function Prototype
+void* ConnectionProcessor(void* ptr);
+
+//Structure
+struct connectionProcessorData {
+    sockaddr_in socket;
+    int repetition;
+};
 
 int main(int argc, char* argv[]) {
     //Local Variables
@@ -19,6 +32,8 @@ int main(int argc, char* argv[]) {
     int repetition = (int)*argv[1];
     sockaddr_in acceptSock;
     const int on = 1;
+    pthread_t thread;
+    sockaddr_in newsock;
 
     bzero((char*) &acceptSock, sizeof(acceptSock));  // zero out the data structure
     acceptSock.sin_family = AF_INET;   // using IP
@@ -37,21 +52,51 @@ int main(int argc, char* argv[]) {
     listen(serverSd, n);  // listen on the socket and allow up to n connections to wait.
 
     // Accept the connection as a new socket
-    sockaddr_in newsock;   // place to store parameters for the new connection
     socklen_t newsockSize = sizeof(newsock);
 
     while (1) {
-        int newSd = accept(serverSd, (sockaddr *)&newsock, &newsockSize);  // grabs the new connection and assigns it a temporary socket
+        //int newSd = accept(serverSd, (sockaddr *)newsock &newsockSize);  // grabs the new connection and assigns it a temporary socket
+
+        //Create the structure here, so each thread has its own structure.
+        connectionProcessorData* structure = new connectionProcessorData;
+        structure->repetition = repetition;
+        structure->socket = newsock; //TODO: improve name.
 
         //Send the input to a thread to deal with it
-        pthread_create(connectionProcessor(newsock));
-
+        pthread_create(&thread, NULL, ConnectionProcessor, &structure);
         // Close the socket
-        close(newSd);
+        //close(newSd); //TODO: MOVE/REMOVE THIS
+
     }
     return 0;
 }
 
-void ConnectionProcessor(sockaddr_in socket) {
+void* ConnectionProcessor(void* ptr) {
+    //Local Variables
+    timeval startTime;
+    timeval endTime;
 
+    //Allocate databuf[BUFSIZE]
+    char databuf[BUFFERSIZE];
+
+    //Start a timer by calling gettimeofday.
+    gettimeofday(&startTime, NULL);
+
+    //Repeat reading data from the client into databuf[BUFSIZE].
+
+    //Stop the timer by calling gettimeofday.
+    gettimeofday(&endTime, NULL);
+
+    //Send the number of read( ) calls made.
+    //write(sd, databuf, nbufs * bufsize);
+
+    //Print out the statistics.
+    //EXAMPLE:   data-receiving time = xxx usec
+
+    //Close this connection.
+
+    //Clean up memory.
+    delete ptr;
+
+    return 0;
 }
